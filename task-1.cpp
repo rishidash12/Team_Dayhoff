e88855d3b69ae215a6c301225b6c4ef6ea6f2d46
rishidash12 <88284390+rishidash12@users.noreply.github.com>
2021-08-02 23:09:50 +0530

    created task-1.cpp

---
+++ task-1.cpp
@@ -0,0 +1,11 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+cout<<"Rishikesh Dash";
+cout<<"rishikeshdash50@gmail.com";
+cout<<"@Rishikesh";
+cout<<"Drug development";
+getch();
+}
