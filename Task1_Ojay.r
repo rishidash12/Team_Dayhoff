4d5fca481cf5541eded3ad860f7a1404873c8798
Tope-nugwa <69086477+Tope-nugwa@users.noreply.github.com>
2021-08-04 09:44:07 +0100

    Add files via upload

---
+++ Task1_Ojay.r
@@ -0,0 +1,37 @@
+#R version 3.4.4 
+
+name <- "Ojonugwa"
+email <- "otabu01@gmail.com"
+slack <- "@Ojay"
+biostack <- "public health and genomic epidermiology"
+twitter <- "@oj_nugwa_"
+
+
+HD <- function(str1, str2){
+
+   str1 <- as.character(str1)
+   str2 <- as.character(str2)
+
+   length.str1 <- nchar(str1)
+   length.str2 <- nchar(str2)
+
+   string.temp1 <- c()
+   for (i in 1:length.str1){
+     string.temp1[i] = substr(str1, start=i, stop=i)
+   }
+   string.temp2 <- c()
+   for (i in 1:length.str2){
+     string.temp2[i] = substr(str2, start=i, stop=i)
+   }
+   return(sum(string.temp1 != string.temp2))
+   }
+
+hamming <- HD(slack,twitter)
+  
+# sum(as.vector(twitter) != as.vector(slack))
+
+
+print(paste(list(name, email, slack, biostack,
+ twitter, hamming)))
+   
+   
