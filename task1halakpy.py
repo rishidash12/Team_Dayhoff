474f96c818eafe4c79ac9930daf5b31671980add
Halak Shukla <88287509+halakshukla@users.noreply.github.com>
2021-08-05 22:31:07 +0530

    Add files via upload

---
+++ task1halakpy.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+name = ("Halak Shukla")
+email = ( "halakshukla0101@gmail.com" )
+slack_Username = ( "@Halak" )
+biostack = ( "Medicinal chemistry and cheminfomatics")
+twitter_Handle = ( "@7halg" )
+def h_d_loop(str_1, str_2):
+     h_distance = 0
+     for position in range(len(str_1)): 
+        if str_1[position] != str_2[position]:
+           h_distance += 1
+     return h_distance 
+print( name, email, slack_Username, biostack, twitter_Handle,h_d_loop(twitter_Handle, slack_Username), sep = ",")
