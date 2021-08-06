e58500ea147e8db72128555cb26f75da0bcd53d0
Tusar Mondal <86488305+Tusar9732@users.noreply.github.com>
2021-08-07 02:30:47 +0530

    Add files via upload

---
+++ dayhoffscript_tusar.pl
@@ -0,0 +1,9 @@
+
+$Name= "Tusar Mondal";
+$Email= "tusarmondal9732@gmail.com";
+$Slack= "@Tusar";
+$Biostacks= "Data science";
+$Twitter= "@Tusar";
+$Hammingdis= "0";
+print("$Name\n","$Email\n","Slack\n","$Biostacks\n","Twitter\n","Hammingdis\n");
+exit
