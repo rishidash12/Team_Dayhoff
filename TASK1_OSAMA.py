2dfd2d90723bd992e0c57361437e1ae6cd3b1634
Osama A. A. Mohamed <88313846+sancho-o@users.noreply.github.com>
2021-08-06 11:09:38 +0200

    Update TASK1_OSAMA.py

--- TASK1_OSAMA.py
+++ TASK1_OSAMA.py
@@ -1,4 +1,13 @@
-print ("Osama A. A. Mohamed")
-print ("osamaaatif@outlook.com")
-print ("@Osama")
-print ("Transcriptomics")
+
+name = ("Osama A. A. Mohamed")
+email = ( "osamaaatif@outlook.com" )
+slack_username = ( "@Osama" )
+biostack = ( "Transcriptomics")
+twitter_handle = ( "@_sancho" )
+def hamming_distance (a, b):
+     distance = 0
+     for i in range(len(a)): 
+        if a[i] != b[i]:
+           distance += 1
+     return distance 
+print (name, email, slack_username, biostack, twitter_handle, hamming_distance(twitter_handle, slack_username), sep = ",")
