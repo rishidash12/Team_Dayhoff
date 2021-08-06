2b415f218f97c0ea67d5d96bca6050e329a45ef6
Tusar Mondal <86488305+Tusar9732@users.noreply.github.com>
2021-08-07 02:23:02 +0530

    Delete dayhoffscript_tusar.pl

--- dayhoffscript_tusar.pl
+++
@@ -1,9 +0,0 @@
-
-$Name= "Tusar Mondal";
-$Email= "tusarmondal9732@gmail.com";
-$Slack= "@Tusar";
-$Biostacks= "Data science";
-$Twitter= "@Tusar";
-$Hammingdis= "0";
-print("$Name\n","$Email\n","$Slack\n","$Biostacks\n","$Twitter\n","$Hammingdis\n");
-exit
