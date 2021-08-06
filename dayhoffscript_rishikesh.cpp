a5c5da020d62704fcf1c9f53694b6709fe5eee8b
Rishikesh Dash <88284390+rishidash12@users.noreply.github.com>
2021-08-07 00:33:30 +0530

    Update and rename task-1_Rishikesh.cpp to dayhoffscript_rishikesh.cpp

---
+++ dayhoffscript_rishikesh.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  char slack_handle[10]="@Rishikesh";
+  char twitter_handle[10]="@Rishidash";
+  int i,hamming_dist=0;
+cout<<"\nRishikesh Dash ";
+cout<<"rishikeshdash50@gmail.com ";
+cout<<"@Rishikesh ";
+cout<<"Drug devlopment ";
+for(i=0;i<=9;i++)
+{
+  if(slack_handle[i]!=twitter_handle[i])
+  {
+    hamming_dist++;
+  }
+}
+  cout<<hamming_dist<<"\n";
+  return 0;
+}
