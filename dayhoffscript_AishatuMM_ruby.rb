d922ba803ef238c4adb02b9926d36f0f3a69a213
Priyanka Singh <55947901+priyankathedecoder@users.noreply.github.com>
2021-08-06 11:03:26 +0530

    Add files via upload

---
+++ dayhoffscript_AishatuMM_ruby.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/ruby -w
+puts "Malami Aishatu Muhammad";
+puts "ummiamal@gmail.com";
+puts "@AishatuMM";
+puts "Genomic or public health and genomic epidemiology";
+puts "@AishatuMM";
+puts "0";
